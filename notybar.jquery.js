53b4157e193d7c33bc8dc20ced601866cffa8f75
Oluwaseye <oluwaseyeo@marriner.com>
2019-03-20 10:08:28 -0400

    Starting out with the NotyBar js file

---
+++ notybar.jquery.js
@@ -0,0 +1,12 @@
+(function ($) { //an IIFE so safely alias jQuery to $
+   
+
+    $.NotyBar.defaultOptions = {
+        position: 'left',
+        blur: false,
+        overlay: true,
+        logs: false
+    };
+
+}(jQuery));
+
